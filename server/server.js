749a43aaac35273cd9005bd1fcd0a0d19c79731f
Vincent Tam <vtam2012@gmail.com>
2023-01-24 20:45:54 -0800

    update server

--- server/server.js
+++ server/server.js
@@ -42,4 +42,4 @@ app.post('/', async (req, res) => {
    }
 })
 
-app.listen("https://chatai-pt2a.onrender.com", () => console.log('Server is runing on https://chatai-pt2a.onrender.com'));
+app.listen(5000, () => console.log('Server is runing on http://localhost:5000'));
